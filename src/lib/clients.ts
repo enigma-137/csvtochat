5449d8e4b25649ccd7a9178115c4f4d8e1eba804
Riccardo Giorato <riccardo.giorato@clipboardhealth.com>
2025-06-17 08:34:15 +0200

    remove codesandbox

--- src/lib/clients.ts
+++ src/lib/clients.ts
@@ -1,6 +1,5 @@
 import Together from "together-ai";
 import { createTogetherAI } from "@ai-sdk/togetherai";
-import { CodeSandbox } from "@codesandbox/sdk";
 
 export const togetherClient = new Together({
   apiKey: process.env.TOGETHER_API_KEY,
@@ -9,6 +8,3 @@ export const togetherClient = new Together({
 export const togetherAISDKClient = createTogetherAI({
   apiKey: process.env.TOGETHER_API_KEY,
 });
-
-// Create the client with your token
-export const csbSdk = new CodeSandbox(process.env.CSB_API_KEY);
